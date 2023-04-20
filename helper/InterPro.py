52c2e688a77775d332a1d642e6ddd17fa0ec1dae
Alekhya Kandoor <adk9hq@bme-c61t2n66.zyxel.com>
2023-04-20 12:44:47 -0400

    Added Interpro ID and output directory as arguments. This script now outputs a csv file with Uniprot IDs to specified location.

--- helper/InterPro.py
+++ helper/InterPro.py
@@ -1,12 +1,15 @@
 import requests
 import logging
+import os
+import requests
+import logging
 
-'''Given an Interpro Accession ID, this class returns a list of Uniprot Accession IDs associated to this Interpro Acc ID'''
+def fetch_uniprotids(InterPro_ID, specie='Homo sapiens'):
     
-def fetch_uniprotid(InterPro_ID, specie='Homo sapiens'):
-    
+    '''Returns a list of Uniprot Accession IDs associated to this Interpro Acc ID'''
+
     fetch_all_results = []
-    
+
     base_url = f"https://www.ebi.ac.uk:443/interpro/api/protein/reviewed/entry/InterPro/{InterPro_ID}/?page_size=200"
 
     logging.basicConfig(filename='error.log',
@@ -29,16 +32,33 @@ def fetch_uniprotid(InterPro_ID, specie='Homo sapiens'):
     except requests.exceptions.RequestException as err:
         print('Found request exceptions...')
         logging.warning(err)
-        
+
     UNIPROT_ID_LIST = []
-    
+
     for entry in fetch_all_results:
-        Uniprot_ACC = (entry['metadata']['accession'])
+        Uniprot_Accession = (entry['metadata']['accession'])
         Scientific_name = entry['metadata']['source_organism']['scientificName']
         fullname = (entry['metadata']['source_organism']['fullName'] )
 
         if Scientific_name == specie:
 
-            UNIPROT_ID_LIST.append(Uniprot_ACC)
-    print('Found {count} Uniprot IDs linked to {IPR_ID}'.format(count=len(UNIPROT_ID_LIST), IPR_ID=InterPro_ID))        
+            UNIPROT_ID_LIST.append(Uniprot_Accession)
+
+    print('Found {count} Uniprot IDs linked to {IPR_ID}'.format(count=len(UNIPROT_ID_LIST), IPR_ID=InterPro_ID))
     return(UNIPROT_ID_LIST)
+
+
+def Main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("InterPro_ID",help="InterPro ID you wish to enter", type=str)
+    parser.add_argument("Output_Dir", help="Directory to write the file with Uniprot IDs", type=str)
+
+    args=parser.parse_args()
+
+    df = pd.DataFrame()
+    df['Uniprot ID'] = fetch_uniprotids(args.InterPro_ID)
+    PATH = args.Output_Dir+'/UniProt_IDs.csv'
+    df.to_csv(PATH, index=False)
+
+if __name__ =='__main__':
+    Main()
