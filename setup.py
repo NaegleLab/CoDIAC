5e1f87c7e9a595e81ca18eb24e090c0acaa1127c
alekhyaa2 <alekhyaa2@gmail.com>
2024-08-03 19:33:18 -0400

    added package versions to setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 # Run setup
 setup(
     name="codiac",
-    version="0.0.3",
+    version="0.0.4",
     author="Naegle Lab",
     author_email="kmn4mj@virginia.edu",
     url="https://github.com/NaegleLab/CoDIAC",
-    install_requires=['pandas', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'statsmodels', 'biopython','requests'],
+    install_requires=['pandas==2.1.*', 'numpy==1.26.*', 'scipy==1.11.*', 'matplotlib==3.7.*', 'seaborn==0.13.*', 'statsmodels==0.14.*', 'biopython==1.79.*','requests==2.31.*', 'cogent3==2023.7.18a1'],
     license='GNU General Public License v3',
     description='CoDIAC: COmprehensive Domain Interface Anlysis of Contacts',
-#    long_description=This is the source code for CoDIAC, an open source Python toolkit for harnessing InterPro, Uniprot, PDB, and AlphaFold for generating references of proteins containing domains of interest and analyzing the contacts that exist between that domain and other regions of interest."""   
+    long_description='This is the source code for CoDIAC, an open source Python toolkit for harnessing InterPro, Uniprot, PDB, and AlphaFold for generating references of proteins containing domains of interest and analyzing the contacts that exist between that domain and other regions of interest.', 
     project_urls = {'Documentation': 'https://naeglelab.github.io/CoDIAC/index.html'},
     classifiers=[
         "Programming Language :: Python :: 3",
